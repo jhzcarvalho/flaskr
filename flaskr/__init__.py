7bb59433748cd6a181bed07ce3ff568a9921e0f4
João Henrique Carvalho <jhcarvalho3@gmail.com>
2024-09-27 15:05:50 -0300

    solving problems in the blog part

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -2,17 +2,17 @@ import os
 
 from flask import Flask
 
+
 def create_app(test_config=None):
     # Create and configure the app
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
-        SECRET_KEY='dev', 
-        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite')
+        SECRET_KEY="dev", DATABASE=os.path.join(app.instance_path, "flaskr.sqlite")
     )
 
     if test_config is None:
         # load the instance config, if it exists, when not testing
-        app.config.from_pyfile('config.py', silent=True)
+        app.config.from_pyfile("config.py", silent=True)
     else:
         # load the test instance folder exists
         app.config.from_mapping(test_config)
@@ -24,8 +24,24 @@ def create_app(test_config=None):
         pass
 
     # a simple page
-    @app.route('/hello')
+    @app.route("/hello")
     def hello():
-        return 'Hello, World!'
-    
+        return "Hello, World!"
+
+    ## it is now possible to create or clean the db from de cli
+    from . import db
+
+    db.init_app(app)
+
+    ## Blueprint and Views
+    from . import auth
+
+    app.register_blueprint(auth.bp)
+
+    ## Blog Blueprint
+    from . import blog
+
+    app.register_blueprint(blog.bp)
+    app.add_url_rule("/", endpoint="index")
+
     return app
